6b4f0ca9727eb33606f8da5044411ed512272c2c
Al Murad <almurad067.com>
2024-11-21 20:27:59 +0600

    first commit

---
+++ abstract_class_and_method/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class_and_method
+{
+    internal class Program
+    {
+        //concept of Abstract class and abstract method
+        public enum Gender { male = 1, female = 2 }
+        public abstract class Person
+        {
+            public string firstname;
+            public string lastname;
+            public int age;
+            public Gender Gender;
+            public abstract void Name();
+        }
+
+        public class Student : Person
+        {
+            public int Roll;
+            public float fees;
+            public override void Name()
+            {
+                //using string interpolation to dispaly varibale
+                string fullname = this.firstname + " " + this.lastname;
+                Console.WriteLine($"student name is {fullname}");
+                Console.WriteLine($"student age is {age}");
+                Console.WriteLine($"student gender is {Gender}");
+                Console.WriteLine($"student roll is {Roll}");
+                Console.WriteLine($"student fees is {fees}");
+            }
+
+
+            public class Teacher : Person
+            {
+                public string Department;
+                public float Salary;
+                public override void Name()
+                {
+                    //using string interpolation to dispaly varibale
+                    string fullname = this.firstname + " " + this.lastname;
+                    Console.WriteLine($"Teacher name is = {fullname}");
+                    Console.WriteLine($"Teacher age is = {age}");
+                    Console.WriteLine($"Teacher gender is = {Gender}");
+                    Console.WriteLine($"Teacher's Department is = {Department}");
+                    Console.WriteLine($"Teacher salary is  = {Salary}");
+
+                }
+
+            }
+           
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Student Info--");
+                Student s = new Student();
+                s.firstname = "Al";
+                s.lastname = "Murad";
+                s.age = 27;
+                s.Gender = Gender.male;
+                s.Roll = 111;
+                s.fees = 670;
+                s.Name();
+                Console.WriteLine("-------------");
+                Console.WriteLine("-------------");
+                Console.WriteLine("Teacher Info--");
+                Teacher t = new Teacher();
+                t.firstname="Abid";
+                t.lastname = "Hassan";
+                t.age = 36;
+                t.Gender = Gender.male;
+                t.Department = "Accounting";
+                t.Salary = 256000;
+                t.Name();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ abstract_class_and_method/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("abstract_class_and_method")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("abstract_class_and_method")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e61f8ba8-6b89-4c69-9332-c310a4da9cdf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
